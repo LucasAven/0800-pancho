40931adee04f5fd7a67a4d11caf075885bb193d4
lavendano <lavendano@vairix.com>
2023-03-06 11:18:47 -0300

    feat: added sign out button in admin panel

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,6 +2,7 @@ import SectionTitle from "components/SectionTitle";
 import Tracks from "components/Tracks";
 import Head from "next/head";
 import { withProtected } from "utils/hoc";
+import { signOut } from "next-auth/react";
 
 const Admin = () => {
 
@@ -10,6 +11,9 @@ const Admin = () => {
        <Head>
          <title>Admin | 0800 Pancho</title>
        </Head>
+       <button className="primary-button ml-auto mr-4" onClick={() => void signOut()}>
+         Cerrar Sesion
+       </button>
        <SectionTitle className="mt-5 mb-auto w-auto text-center font-bold text-baseText">
          Admin Panel
        </SectionTitle>
